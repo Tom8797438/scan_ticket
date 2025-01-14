6f151ae76ee2914b32d1cc875bf79728862e62d4
Tom8797438 <tomzu974@gmail.com>
2025-01-14 16:23:42 +0400

    ameliorations

--- scan-ticket-app/src/components/services/api.js
+++ scan-ticket-app/src/components/services/api.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-// Utilise la variable d'environnement pour configurer l'URL de base
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // URL de l'API définie dans .env
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+// // Utilise la variable d'environnement pour configurer l'URL de base
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL, // URL de l'API définie dans .env
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
-export default api;
+// export default api;
