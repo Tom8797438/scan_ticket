763e33484b4d64cd1f703deb185633a748e9bb74
Tom8797438 <tomzu974@gmail.com>
2025-01-20 14:00:15 +0400

    jenkins update 6

--- scan-ticket-app/vite.config.js
+++ scan-ticket-app/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   // server: {
@@ -14,6 +15,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     //vueDevTools(),
+
   ],
   resolve: {
     alias: {
